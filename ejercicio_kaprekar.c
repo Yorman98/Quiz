2987128f1c53c2cd4dd1f9563634f2516d3698c8
Yorman98 <yormanjosuegalindopastran@gmail.com>
2016-08-03 11:10:51 -0400

    Terminado

--- ejercicio_kaprekar.c
+++ ejercicio_kaprekar.c
@@ -1,22 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdlib.h>
+
+
 
 int main() {
- int n,u,d,c,um,u2,d2,c2,um2,total,total2;
+ int n,u,d,c,um,u2,d2,c2,um2,total,total2,total3,kaprekar,NV,salida,n2;
  char ini;
 
 
 
+kaprekar=6174;
+
     printf("Bienvenido \nquieres iniciar el programa? s/n \n");
     scanf("%c",&ini);
+    system("cls");
+
         do
         {
 
             if(ini== 's' || ini=='S'){
-
+            n=0; NV=0;salida=0;
             printf("introduce un numero de 4 digitos: ");
+            fflush(stdin);
             scanf("%d",&n);
+            n2=n;
+
+              if(n>9999){
+            printf("\nValor no valido\n");
+            }
+            else{
+                if(n==6174){
+                    printf("\nKaprekar \n");
+                }
+                else{
+                   do{
+
                     um = n/1000;
                     n = n%1000;
                     um2=um;
@@ -31,16 +49,18 @@ int main() {
                                 n = n%1;
                                 u2=u;
                         if(u==d && d==c && c==um){
-                            printf("repdigit");
+                            salida=1;
                         }
-                        //descendente
+
+                        else{
+                     //descendente
                         if( um<=c && c<=d && d<=u){
                            um=um*1;
                            c=c*10;
                            d=d*100;
                            u=u*1000;
                             total=um+c+d+u;
-                            printf("%d \n",total); // um,c,d,u
+                           // um,c,d,u
                         }
                         else if(um <= c && c<=u && u<=d){
                            um=um*1;
@@ -48,7 +68,7 @@ int main() {
                            u=u*100;
                            d=d*1000;
                             total=um+c+d+u;
-                           printf("%d\n",total); //um,c,u,d
+                           //um,c,u,d
                         }
                         else if (um<=d && d<=c && c<=u){
                            um=um*1;
@@ -56,7 +76,7 @@ int main() {
                            c=c*100;
                            u=u*1000;
                            total=um+c+d+u;
-                            printf("%d\n",total); //um,d,c,u
+                            //um,d,c,u
                         }
                         else if (um<=u && u<=c && c<=d){
                            um=um*1;
@@ -64,7 +84,7 @@ int main() {
                            c=c*100;
                            d=d*1000;
                            total=um+c+d+u;
-                           printf("%d",total); //um,u,c,d
+                          ; //um,u,c,d
                         }
                         else if (um<=d && d<=u && u<=c){
                            um=um*1;
@@ -73,7 +93,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //um,d,u,c
+                          //um,d,u,c
                         }
                         else if (um<=u && u<=d && d<=c){
                            um=um*1;
@@ -82,7 +102,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //um,u,d,c
+                         //um,u,d,c
                         }
                           else if (c<=um && um<=d && d<=u){
                            um=um*10;
@@ -91,7 +111,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,um,d,u
+                           //c,um,d,u
 
                         }
                          else if (c<=um && um<=u && u<=d){
@@ -101,7 +121,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,um,u,d
+                           //c,um,u,d
                         }
                           else if (d<=um && um<=c && c<=u){
                            um=um*10;
@@ -110,7 +130,7 @@ int main() {
                            c=c*100;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,um,c,u
+                           //d,um,c,u
                         }
                          else if (u<=um && um<=c && c<=d){
                            um=um*10;
@@ -119,7 +139,7 @@ int main() {
                            c=c*100;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,um,c,d
+                           //u,um,c,d
                         }
                            else if (d<=um && um<=u && u<=c){
                            um=um*10;
@@ -128,7 +148,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,um,u,c
+                           //d,um,u,c
                         }
                             else if (u<=um && um<=d && d<=c){
                            um=um*10;
@@ -137,7 +157,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,um,d,c
+                           //u,um,d,c
                         }
                             else if (c<=d && d<=um && um<=u){
                            um=um*100;
@@ -146,7 +166,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,d,um,u
+                            //c,d,um,u
                         }
                              else if (c<=u && u<=um && um<=d){
                            um=um*100;
@@ -155,7 +175,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,u,um,d
+                            //c,u,um,d
                         }
                               else if (d<=c && c<=um && um<=u){
                            um=um*100;
@@ -164,7 +184,7 @@ int main() {
                            c=c*10;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,c,um,u
+                            //d,c,um,u
                         }
                               else if (u<=c && c<=um && um<=d){
                            um=um*100;
@@ -173,7 +193,7 @@ int main() {
                            c=c*10;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,c,um,d
+                            //u,c,um,d
                         }
                               else if (d<=u && u<=um && um<=c){
                            um=um*100;
@@ -182,7 +202,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,u,um,c
+                            //d,u,um,c
                         }
                                  else if (u<=d && d<=um && um<=c){
                            um=um*100;
@@ -191,7 +211,7 @@ int main() {
                            c=c*1000;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,d,um,c
+                            //u,d,um,c
                         }
                                  else if (c<=d && d<=u && u<=um){
                            um=um*1000;
@@ -200,7 +220,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,d,u,um
+                           //c,d,u,um
                         }
                                  else if (c<=u && u<=d && d<=um){
                            um=um*1000;
@@ -209,7 +229,7 @@ int main() {
                            c=c*1;
 
                            total=um+c+d+u;
-                           printf("%d",total); //c,u,d,um
+                           //c,u,d,um
                         }
                                else if (u<=c && c<=d && d<=um){
                            um=um*1000;
@@ -218,7 +238,7 @@ int main() {
                            c=c*10;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,c,d,um
+                           //u,c,d,um
                         }
                                 else if (d<=c && c<=u && u<=um){
                            um=um*1000;
@@ -227,7 +247,7 @@ int main() {
                            c=c*10;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,c,u,um
+                            //d,c,u,um
                         }
                             else if (d<=u && u<=c && c<=um){
                            um=um*1000;
@@ -236,7 +256,7 @@ int main() {
                            c=c*100;
 
                            total=um+c+d+u;
-                           printf("%d",total); //d,u,c,um
+                            //d,u,c,um
                         }
                                else if (u<=d && d<=c && c<=um){
                            um=um*1000;
@@ -245,11 +265,8 @@ int main() {
                            c=c*100;
 
                            total=um+c+d+u;
-                           printf("%d",total); //u,d,c,um
+                          //u,d,c,um
                         }
-
-                        printf("\n%d \n",total-100);
- printf("\n-----------------------------------------------\n");
                          //Ascendente
                         if( um2>=c2 && c2>=d2 && d2>=u2){
                            um2=um2*1;
@@ -257,7 +274,7 @@ int main() {
                            d2=d2*100;
                            u2=u2*1000;
                             total2=um2+c2+d2+u2;
-                            printf("%d \n",total2); //u,d,c,um
+                             //u,d,c,um
                         }
                         else if(um2>=c2 && c2>=u2 && u2>=d2){
                            um2=um2*1;
@@ -265,7 +282,7 @@ int main() {
                            u2=u2*100;
                            d2=d2*1000;
                             total2=um2+c2+d2+u2;
-                           printf("%d\n",total2); //d,u,c,um
+                            //d,u,c,um
                         }
                         else if (um2>=d2 && d2>=c2 && c2>=u2){
                            um2=um2*1;
@@ -273,7 +290,7 @@ int main() {
                            c2=c2*100;
                            u2=u2*1000;
                            total2=um2+c2+d2+u2;
-                            printf("%d\n",total2); //u,c,d,um
+                            //u,c,d,um
                         }
                         else if (um2>=u2 && u2>=c2 && c2>=d2){
                            um2=um2*1;
@@ -281,7 +298,7 @@ int main() {
                            c2=c2*100;
                            d2=d2*1000;
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //d,c,u,um
+                            //d,c,u,um
                         }
                         else if (um2>=d2 && d2>=u2 && u2>=c2){
                            um2=um2*1;
@@ -290,7 +307,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,u,d,um
+                            //c,u,d,um
                         }
                         else if (um2>=u2 && u2>=d2 && d2>=c2){
                            um2=um2*1;
@@ -299,7 +316,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,d,u,um
+                            //c,d,u,um
                         }
                           else if (c2>=um2 && um2>=d2 && d2>=u2){
                            um2=um2*10;
@@ -308,7 +325,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //u,d,um,c
+                           //u,d,um,c
 
                         }
                          else if (c2>=um2 && um2>=u2 && u2>=d2){
@@ -318,7 +335,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //d,u,um,c
+                            //d,u,um,c
                         }
                           else if (d2>=um2 && um2>=c2 && c2>=u2){
                            um2=um2*10;
@@ -327,7 +344,7 @@ int main() {
                            c2=c2*100;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //u,c,um,d
+                           //u,c,um,d
                         }
                          else if (u2>=um2 && um2>=c2 && c2>=d2){
                            um2=um2*10;
@@ -336,7 +353,7 @@ int main() {
                            c2=c2*100;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //d,c,um,u
+                            //d,c,um,u
                         }
                            else if (d2>=um2 && um2>=u2 && u2>=c2){
                            um2=um2*10;
@@ -345,7 +362,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,u,um,d
+                            //c,u,um,d
                         }
                             else if (u2>=um2 && um2>=d2 && d2>=c2){
                            um2=um2*10;
@@ -354,7 +371,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,d,um,u
+                           //c,d,um,u
                         }
                             else if (c2>=d2 && d2>=um2 && um2>=u2){
                            um2=um2*100;
@@ -363,7 +380,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //u,um,d,c
+                            //u,um,d,c
                         }
                              else if (c2>=u2 && u2>=um2 && um2>=d2){
                            um2=um2*100;
@@ -372,7 +389,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //d,um,u,c
+                            //d,um,u,c
                         }
                               else if (d2>=c2 && c2>=um2 && um2>=u2){
                            um2=um2*100;
@@ -381,7 +398,7 @@ int main() {
                            c2=c2*10;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //u,um,c,d
+                            //u,um,c,d
                         }
                               else if (u2>=c2 && c2>=um2 && um2>=d2){
                            um2=um2*100;
@@ -390,7 +407,7 @@ int main() {
                            c2=c2*10;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //d,um,c,u
+                            //d,um,c,u
                         }
                               else if (d2>=u2 && u2>=um2 && um2>=c2){
                            um2=um2*100;
@@ -399,7 +416,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,um,u,d
+                            //c,um,u,d
                         }
                             else if (u2>=d2 && d2>=um2 && um2>=c2){
                            um2=um2*100;
@@ -408,7 +425,7 @@ int main() {
                            c2=c2*1000;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //c,um,d,u
+                         //c,um,d,u
                         }
                                  else if (c2>=d2 && d2>=u2 && u2>=um2){
                            um2=um2*1000;
@@ -417,7 +434,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,u,d,c
+                            //um,u,d,c
                         }
                                  else if (c2>=u2 && u2>=d2 && d2>=um2){
                            um2=um2*1000;
@@ -426,7 +443,7 @@ int main() {
                            c2=c2*1;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,d,u,c
+                            //um,d,u,c
                         }
                                else if (u2>=c2 && c2>=d2 && d2>=um2){
                            um2=um2*1000;
@@ -435,7 +452,7 @@ int main() {
                            c2=c2*10;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,d,c,u
+                            //um,d,c,u
                         }
                                    else if (d2>=c2 && c2>=u2 && u2>=um2){
                            um2=um2*1000;
@@ -444,7 +461,7 @@ int main() {
                            c2=c2*10;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,u,c,d
+                           //um,u,c,d
                         }
                             else if (d2>=u2 && u2>=c2 && c2>=um2){
                            um2=um2*1000;
@@ -453,7 +470,7 @@ int main() {
                            c2=c2*100;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,c,u,d
+                            //um,c,u,d
                         }
                                else if (u2>=d2 && d2>=c2 && c2>=um2){
                            um2=um2*1000;
@@ -462,16 +479,29 @@ int main() {
                            c2=c2*100;
 
                            total2=um2+c2+d2+u2;
-                           printf("%d",total2); //um,c,d,u
+                            //um,c,d,u
                         }
+                        if(n != kaprekar){
+                        n=total-total2;
+                         printf("\n%d - %d= %d\n",total,total2,n);
+                        }
+                        NV++;
 
-                        printf("\n%d \n",total2-100);
-
+                        if(n==kaprekar) salida=1;
+                        }
+                   }while(!salida);
+                  if(n==kaprekar){
+                  printf("\nla cantidad de veces a repetir el numero %d para la constante kaprekar es: %d\n",n2,NV);
+                   }
+                    else{
+                    printf("\nNumero repdigit \n");
+                    }
+            }// cierra el else de la condicion kaprekar
             }// if de introducir numeros
 
+            }//cierra el if de mayor de 4 digitos
 
-
-                printf("Quieres seguir em el programa? s/n \n");
+                printf("\nQuieres seguir en el programa? s/n \n");
                 fflush(stdin);
                 scanf("%c",&ini);
                 system("cls");
